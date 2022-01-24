85f210031147a9648d4bbc678e0df9a9d726c54d
alparslanustaa <alparslanustaa@gmail.com>
2022-01-24 23:01:39 +0000

    Sending HTML

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -17,4 +18,8 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
