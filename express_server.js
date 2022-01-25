651ea4815880c5955ef814dc7f36334f30addffa
alparslanustaa <alparslanustaa@gmail.com>
2022-01-25 23:34:31 +0000

    edit Post and btn edit

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 
 
 const urlDatabase = {
-  b2xVn2 : "http://www.lighthouselabs.ca",
+  "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
@@ -45,16 +45,27 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: req.params.longURL,
+    longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  var short = req.params.shortURL;
+  let short = req.params.shortURL;
   let longURL = urlDatabase[short];
   res.redirect(longURL);
 });
+
+app.get("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let longURL = urlDatabase[shortURL];
+  let templateVars = {
+    shortURL: shortURL,
+    longURL: longURL,
+  };
+  res.render("urls_show", templateVars);
+});
+
 
 app.post("/urls", (req, res) => {
   let url = generateRandomString();
@@ -68,11 +79,24 @@ app.post("/urls/:shortURL/delete", (req,res)=> {
   res.redirect("/urls/");
 });
 
+app.post('/urls/:shortURL/edit', (req, res) => {   
+  const shortURL = req.params.shortURL;   
+  res.redirect(`/urls/${shortURL}`); 
+});
+
+
+app.post("/urls/:id", (req, res) => {
+  const longURL = req.body.longURL;
+  urlDatabase[req.params.id] = longURL;
+  res.redirect("/urls/");
+});
+
+
 function generateRandomString() {
-  var randomChars =
+  const randomChars =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var result = "";
-  for (var i = 0; i < 6; i++) {
+  const result = "";
+  for (let i = 0; i < 6; i++) {
     result += randomChars.charAt(
       Math.floor(Math.random() * randomChars.length)
     );
