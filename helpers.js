dbc348984737d9c30e82b2aea26e8e8f8d6246f3
alparslanustaa <alparslanustaa@gmail.com>
2022-01-28 20:07:50 +0000

    helper function moved

---
+++ helpers.js
@@ -0,0 +1,11 @@
+//email lookup helper function
+function getUserByEmail(email, database) {
+  for (let key in database) {
+    if (database[key].email === email) {
+      return database[key];
+    }
+  }
+  return undefined;
+}
+
+module.exports = {getUserByEmail};
